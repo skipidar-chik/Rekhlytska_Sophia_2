2dc48188911758c8f50e791dccea323637c05766
skipidar-chik <sofiarehl13@gmail.com>
2022-06-21 15:42:55 +0300

    chhhh

---
+++ op_lr_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Холм");
+TerrainProfile holm = new();
+holm.altitudes = new List<double>() { 210.5, 220, 215.5, 216, 230.2, 219 };
+holm.Laing = 10;
+holm.PrintAll();
+Console.WriteLine("---------------------------------------");
+holm.Compare(new List<double>() { 350.1, 315, 380.5, 385, 364, 360.2 });
+Serealizer();
+
+async void Serealizer()
+{
+    using FileStream fs = new FileStream("TerrainProfile.json", FileMode.OpenOrCreate);
+        await JsonSerializer.SerializeAsync<TerrainProfile>(fs, holm);
+}
